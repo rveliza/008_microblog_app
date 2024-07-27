732b67ed62afc2be1a8eb1e051e3558d917637c1
Reyner Veliz <reyner@reynerveliz.com>
2024-07-26 22:46:36 -0600

    Format dates correctly

--- app.py
+++ app.py
@@ -13,4 +13,13 @@ def home():
         entry_content = request.form.get('content')
         formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
         entries.append((entry_content, formatted_date))
-    return render_template("home.html", entries=entries)
+
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.today().strftime('%b %d')
+        )
+        for entry in entries
+    ]
+    return render_template("home.html", entries=entries_with_date)
