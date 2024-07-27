44e0b00e07d2e602a347c73be90ed093f7e60cd7
Reyner Veliz <reyner@reynerveliz.com>
2024-07-26 22:27:30 -0600

    Display past entries

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import datetime
 
 app = Flask(__name__)
 
+entries = []
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get('content')
         formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
-        print(entry_content, formatted_date)
-    return render_template("home.html")
+        entries.append((entry_content, formatted_date))
+    return render_template("home.html", entries=entries)
