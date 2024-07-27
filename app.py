92a1ad793cec2a882e8f8fe5e8068d76d0d854ab
Reyner Veliz <reyner@reynerveliz.com>
2024-07-27 00:23:46 -0600

    Retrieve data from MongoDb

--- app.py
+++ app.py
@@ -9,15 +9,13 @@ mongo_pwd = "rLk3hkEOVkSg1tOm"
 client = MongoClient(f"mongodb+srv://reyner:{mongo_pwd}@microblogcluster.eoxsbey.mongodb.net/?retryWrites=true&w=majority&appName=MicroblogCluster" )
 app.db = client.microblog
 
-entries = []
-
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    # db_entries = [e for e in app.db.entries.find({})]
     if request.method == "POST":
         entry_content = request.form.get('content')
         formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
-        entries.append((entry_content, formatted_date))
         app.db.entries.insert_one({
             'content': entry_content,
             'date': formatted_date
@@ -25,10 +23,11 @@ def home():
 
     entries_with_date = [
         (
-            entry[0],
-            entry[1],
+            entry['content'],
+            entry['date'],
             datetime.datetime.today().strftime('%b %d')
         )
-        for entry in entries
+        # for entry in db_entries
+        for entry in app.db.entries.find({})
     ]
     return render_template("home.html", entries=entries_with_date)
